88c471a6bf55e90f068b634b5725651b4c14a8d8
SeolYoungKim <nasur4da@gmail.com>
2024-12-27 10:17:21 +0900

    test: 셔플링 전략에 따른 테스트 추가

--- src/test/kotlin/domain/CellsTest.kt
+++ src/test/kotlin/domain/CellsTest.kt
@@ -3,10 +3,19 @@ package domain
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.equals.shouldNotBeEqual
+import io.kotest.matchers.shouldBe
+import kotlin.random.Random
 
 object NoneShufflingStrategy : ShufflingStrategy {
     override fun shuffle(cells: List<Cell>): List<Cell> {
         return cells
+    }
+}
+
+object FixedSeedShufflingStrategy : ShufflingStrategy {
+    override fun shuffle(cells: List<Cell>): List<Cell> {
+        return cells.shuffled(Random(10))
     }
 }
 
@@ -24,6 +33,29 @@ class CellsTest : FreeSpec({
         cells.elements shouldHaveSize safeCellsCount + landMinesCount
         cells.elements.filterIsInstance<SafeCell>() shouldHaveSize safeCellsCount
         cells.elements.filterIsInstance<LandMineCell>() shouldHaveSize landMinesCount
+    }
+
+    "셔플링 전략에 따라 cells를 섞을 수도 있다" {
+        val safeCellsCount = 95
+        val landMinesCount = 5
+
+        val noneShufflingCells = Cells.create(
+            safeCellsCount = safeCellsCount,
+            landMinesCount = landMinesCount,
+            shufflingStrategy = NoneShufflingStrategy
+        )
+
+        val shufflingCells = Cells.create(
+            safeCellsCount = safeCellsCount,
+            landMinesCount = landMinesCount,
+            shufflingStrategy = FixedSeedShufflingStrategy
+        )
+
+        val noneShufflingElements = noneShufflingCells.elements
+        val shufflingElements = shufflingCells.elements
+
+        noneShufflingElements.size shouldBe shufflingElements.size
+        noneShufflingElements.map { it.javaClass } shouldNotBeEqual shufflingElements.map { it.javaClass }
     }
 
     "SafeCell 개수가 0이하이거나 LandMineCell 개수가 0 이하인 경우 예외를 발생시킨다" - {
