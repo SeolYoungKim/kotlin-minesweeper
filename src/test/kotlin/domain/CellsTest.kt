7d242b957e053a9ec48d3dbf512367af222a84f4
SeolYoungKim <nasur4da@gmail.com>
2024-12-31 23:44:04 +0900

    test: withData를 사용하도록 변경

--- src/test/kotlin/domain/CellsTest.kt
+++ src/test/kotlin/domain/CellsTest.kt
@@ -2,6 +2,7 @@ package domain
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.datatest.withData
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.equals.shouldNotBeEqual
 import io.kotest.matchers.shouldBe
@@ -59,25 +60,21 @@ class CellsTest : FreeSpec({
     }
 
     "SafeCell 개수가 0이하이거나 LandMineCell 개수가 0 이하인 경우 예외를 발생시킨다" - {
-        listOf(
-            1 to 0,
-            0 to 1,
-            0 to 0,
-            -1 to 1,
-            1 to -1,
-            -1 to -1,
-        ).forEach { pair ->
-            val safeCellsCount = pair.first
-            val landMinesCount = pair.second
-
-            "입력값: safeCellsCount=${safeCellsCount}, landMinesCount=${landMinesCount}" {
-                shouldThrow<IllegalArgumentException> {
-                    Cells.create(
-                        safeCellsCount = safeCellsCount,
-                        landMinesCount = landMinesCount,
-                        shufflingStrategy = NoneShufflingStrategy
-                    )
-                }
+        data class Count(val safeCellsCount: Int, val landMinesCount: Int)
+        withData(
+            Count(1, 0),
+            Count(0, 1),
+            Count(0, 0),
+            Count(-1, 1),
+            Count(1, -1),
+            Count(-1, -1),
+        ) { (safeCellsCount, landMinesCount) ->
+            shouldThrow<IllegalArgumentException> {
+                Cells.create(
+                    safeCellsCount = safeCellsCount,
+                    landMinesCount = landMinesCount,
+                    shufflingStrategy = NoneShufflingStrategy
+                )
             }
         }
     }
